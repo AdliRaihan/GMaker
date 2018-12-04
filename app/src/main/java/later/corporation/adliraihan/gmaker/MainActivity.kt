fa0fc7aa5e241eb18680bf7fd51c548418840812
Adli Raihan <43267304+DNoir@users.noreply.github.com>
2018-12-04 12:38:56 +0700

    Update 1.0001
    Fix Bug WhiteScreen diSplashscreen

--- app/src/main/java/later/corporation/adliraihan/gmaker/MainActivity.kt
+++ app/src/main/java/later/corporation/adliraihan/gmaker/MainActivity.kt
@@ -3,6 +3,7 @@ package later.corporation.adliraihan.gmaker
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,9 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thrower)
 
-        //Thrower ke First Run APP
-        Thread.sleep(2000)
+        //Thrower ke First Run APP [Update Later]
         var InterGlobal = Intent(this, FirstRunActivity::class.java)
-        startActivity(InterGlobal)
+        Handler().postDelayed({
+                startActivity(InterGlobal)
+                this.finish()
+        },2000)
     }
 }
