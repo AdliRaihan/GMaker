8fc002016bcc668ad43f0438dc56e57eec0ff7f7
Adli Raihan <43267304+DNoir@users.noreply.github.com>
2018-12-04 15:25:19 +0700

    Update 1.0012
     - Penambahan Activity Register
     - Fixing bug layout cross platform di landing first App

--- app/src/main/java/later/corporation/adliraihan/gmaker/FirstRunActivity.kt
+++ app/src/main/java/later/corporation/adliraihan/gmaker/FirstRunActivity.kt
@@ -3,11 +3,18 @@ package later.corporation.adliraihan.gmaker
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class FirstRunActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Login Activity
+        buttonActivity_Login.setOnClickListener(){
+
+        }
+
     }
 }
